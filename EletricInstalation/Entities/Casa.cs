9f2c4faff5f0828d385f3c6a35c39ebbd55102cf
Augusto Omena <augusto.omena.n@gmail.com>
2023-02-18 20:14:41 -0300

    Adicionado a possibilidade de trabalhar com todos os comodos e fazer calculos para cada um deles.

--- EletricInstalation/Entities/Casa.cs
+++ EletricInstalation/Entities/Casa.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using System.IO.Compression;
 
 namespace EletricInstalation.Entities
 {
@@ -12,26 +13,61 @@ namespace EletricInstalation.Entities
         public double Altura { get; set; } = 0;
         public double Largura { get; set; } = 0;
         public double Total { get; set; } = 0;
-        public int Com { get; set; } = 0;
-        public string Comod { get; set; }
+        public int Com1 { get; set; } = 0;
+        public int Com2 { get; set; } = 0;
+        public int Com3 { get; set; } = 0;
+
+        public int Repet { get; set; } = 0;
+        public string Comod { get; set; } = null;
+        public List<int> ComodList { get; set; } = new List<int> ();
 
         public void Comodos()
         {
-            Console.WriteLine("Quantos comodos?");
-            Com = int.Parse(Console.ReadLine());
-            Comod = "do comodo";
+            Console.WriteLine("Quantos salas?");
+            Com1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos quartos?");
+            Com2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas cozinhas?");
+            Com3 = int.Parse(Console.ReadLine());
+            ComodList.Add(Com1);
+            ComodList.Add(Com2);
+            ComodList.Add(Com3);
         }
+        
         public void Tot()
         {
             try
             {
-                for (int i = 1; i <= Com; i++)
+                int sal = ComodList[0];
+                int qua = ComodList[1];
+                int coz = ComodList[2];
+                for (int go = 0; go <= 2; go++)
                 {
-                    Console.Write($"Digite a Altura {Comod} {i}: ");
-                    Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    Console.Write($"Digite a Largura {Comod} {i}: ");
-                    Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                    Total += Altura * Largura;
+                    int Q = ComodList[go] + 1;
+                    if (go == 0)
+                    {
+                        Comod = "da Sala";
+                    }
+                    if (go == 1)
+                    {
+                        Comod = "do Quarto";
+                    }
+                    if (go == 2)
+                    {
+                        Comod = "da Cozinha";
+                    }
+
+                    for (int i = 1; i < Q; i++)
+                    {
+
+                        Console.Write($"Digite a Altitude A {Comod} {i}: ");
+                        Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        Console.Write($"Digite a Latitude L {Comod} {i}: ");
+                        Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        Console.WriteLine("--------------------------------------------------------------");
+                        Total += Altura * Largura;
+
+                    }
                 }
             }
             catch
